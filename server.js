9ed4ecca2c55abdcf478c4c49d8cc06e6773fafb
Daniel Loureiro Cintra <cintra_daniel@ERIKAs-MacBook-Pro.local>
2022-06-02 15:49:36 -0300

    trying to fix the port problem

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 let PORT = process.env.API_YELLOWPAGES_API_PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("The yellow pages api is working on: " + port);
+  console.log("The yellow pages api is working on: " + PORT);
 });
