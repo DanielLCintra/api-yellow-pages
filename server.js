2438656215837524f919a9845d9022928ee8ec9d
Daniel Loureiro Cintra <cintra_daniel@ERIKAs-MacBook-Pro.local>
2022-06-02 15:47:58 -0300

    trying to fix the port problem

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 
-let port = process.env.API_YELLOWPAGES_API_PORT || 3000;
+let PORT = process.env.API_YELLOWPAGES_API_PORT || 3000;
 
-app.listen(port, () => {
-  console.log("The yellow pages api is working on: http://localhost:" + port);
+app.listen(PORT, () => {
+  console.log("The yellow pages api is working on: " + port);
 });
